5e0bfe6271c8c2e0c64cce6bb5006f36abe36814
DBinK <DBinKv1@Gmail.com>
2024-03-27 01:35:32 +0800

    feat✨:  实时获取热搜

--- task1/index.js
+++ task1/index.js
@@ -1,11 +1,11 @@
 function searchBaidu() {
-        // 获取输入框中的内容
-        var keywords = document.getElementById('searchInput').value;
-        // 将关键词填入URL中
-        var searchUrl = 'https://www.baidu.com/s?wd=' + keywords;
-        // 打开链接
-        window.open(searchUrl);
-    }
+    // 获取输入框中的内容
+    var keywords = document.getElementById('searchInput').value;
+    // 将关键词填入URL中
+    var searchUrl = 'https://www.baidu.com/s?wd=' + keywords;
+    // 打开链接
+    window.open(searchUrl);
+}
 function handleKeyDown(event) {
     if (event.key === "Enter") {
         // 处理回车事件
@@ -25,3 +25,48 @@ function rotateBtn() {
         { once: true }
     );
 }
+
+
+function fillHotList(data) {
+    const hotList = document.querySelector('.hot-list');
+
+    // 清空热搜列表
+    hotList.innerHTML = '';
+
+    // 填充热搜标题
+    data.slice(0, 6).forEach((item, index) => {
+        const { title } = item;
+
+        const li = document.createElement('li');
+
+        const a = document.createElement('a');
+        a.classList.add(`hot-link-${index}`);
+        a.href = `#${index}`;
+
+        const spanNum = document.createElement('span');
+        spanNum.classList.add(`hot-num-${index}`);
+        spanNum.textContent = index + 1;
+
+        const spanTitle = document.createElement('span');
+        spanTitle.classList.add(`hot-title-${index}`);
+        spanTitle.textContent = title;
+
+        a.appendChild(spanNum);
+        a.appendChild(spanTitle);
+
+        li.appendChild(a);
+
+        hotList.appendChild(li);
+    });
+}
+
+// 调用API获取数据
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch("https://v2.api-m.com/api/baiduhot", requestOptions)
+    .then(response => response.json())
+    .then(result => fillHotList(result.data))
+    .catch(error => console.log('error', error));
